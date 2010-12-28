ce7f76b836a454bbd92c6d6b60ddecb40e35c160
Arild <arild.fines@broadpark.no>
2010-12-28 22:51:15 +0100

    Set up initial project structure

---
+++ src/Bluetooth/SerialRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace Rogue.HeartTracker.Bluetooth
+{
+    public class SerialRecord
+    {
+        public byte[] Data { get; private set; }
+
+        public SerialRecord(byte[] data)
+        {
+            Data = data;
+        }
+    }
+}
